fdfa2648deb223ce9a774b3eb44dce3aad1da261
fgoraz <fgoras13@gmail..com>
2022-09-25 22:44:25 +0000

    'deploy'

---
+++ js/about.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkquiz"]=self["webpackChunkquiz"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(6252);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function i(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var c=t(3744);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
